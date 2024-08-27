2553b7a0310fd8de0ce6ac9808e332ba73e6c4c2
mahendrababu4625 <mahendrababu224@gmail.com>
2024-08-27 11:56:51 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Even{
+    int num;
+    public static void main(string[]args){
+        Scanner sc=new Scanner(System.in)
+        System.out.println("enter any value");
+         num=sc.nextInt();
+         if(num%2==0)
+         {
+            System.out.println("given number is even");
+         }
+         else{
+            System.out.println("given number is odd");
+         }
+
+    }
+}
